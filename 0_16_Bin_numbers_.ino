6c4e55bb9a412333a71890b454b1670cd66754ec
Nassor JA <rossan1tech@gmail.com>
2023-08-14 15:06:07 +0300

    Create 0_16_Bin_numbers_.ino
    Code to light up LEDs binary values 0 to 15.

---
+++ 0_16_Bin_numbers_.ino
@@ -0,0 +1,131 @@
+// Define pin numbers for LEDs
+  const int redA = 13;
+  const int greenA = 12;
+  const int redB = 8;
+  const int greenB = 2;
+
+  // Define delay time
+  const int delayTym = 2000;
+  
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(redA, OUTPUT);
+  pinMode(greenA, OUTPUT);
+  pinMode(redB, OUTPUT);
+  pinMode(greenB, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(redA, LOW);
+  digitalWrite(greenA, LOW);
+  digitalWrite(redB, LOW);
+  digitalWrite(greenB, LOW);
+
+  delay(delayTym); // Delay for 1000 milliseconds
+
+  digitalWrite(redA, LOW);
+  digitalWrite(greenA, LOW);
+  digitalWrite(redB, LOW);
+  digitalWrite(greenB, HIGH);
+
+  delay(delayTym); // Delay for 1000 milliseconds
+
+  digitalWrite(redA, LOW);
+  digitalWrite(greenA, LOW);
+  digitalWrite(redB, HIGH);
+  digitalWrite(greenB, LOW);
+
+  delay(delayTym); // Delay for 1000 milliseconds
+
+  digitalWrite(redA, LOW);
+  digitalWrite(greenA, LOW);
+  digitalWrite(redB, HIGH);
+  digitalWrite(greenB, HIGH);
+
+  delay(delayTym); // Delay for 1000 milliseconds
+
+  digitalWrite(redA, LOW);
+  digitalWrite(greenA, HIGH);
+  digitalWrite(redB, LOW);
+  digitalWrite(greenB, LOW);
+
+  delay(delayTym); // Delay for 1000 milliseconds
+
+  digitalWrite(redA, LOW);
+  digitalWrite(greenA, HIGH);
+  digitalWrite(redB, LOW);
+  digitalWrite(greenB, HIGH);
+
+  delay(delayTym); // Delay for 1000 milliseconds
+
+  digitalWrite(redA, LOW);
+  digitalWrite(greenA, HIGH);
+  digitalWrite(redB, HIGH);
+  digitalWrite(greenB, LOW);
+
+  delay(delayTym); // Delay for 1000 milliseconds
+
+  digitalWrite(redA, LOW);
+  digitalWrite(greenA, HIGH);
+  digitalWrite(redB, HIGH);
+  digitalWrite(greenB, HIGH);
+
+  delay(delayTym); // Delay for 1000 milliseconds
+
+  digitalWrite(redA, HIGH);
+  digitalWrite(greenA, LOW);
+  digitalWrite(redB, LOW);
+  digitalWrite(greenB, LOW);
+
+  delay(delayTym); // Delay for 1000 milliseconds
+
+  digitalWrite(redA, HIGH);
+  digitalWrite(greenA, LOW);
+  digitalWrite(redB, LOW);
+  digitalWrite(greenB, HIGH);
+
+  delay(delayTym); // Delay for 1000 milliseconds
+
+  digitalWrite(redA, HIGH);
+  digitalWrite(greenA, LOW);
+  digitalWrite(redB, HIGH);
+  digitalWrite(greenB, LOW);
+
+  delay(delayTym); // Delay for 1000 milliseconds
+
+  digitalWrite(redA, HIGH);
+  digitalWrite(greenA, LOW);
+  digitalWrite(redB, HIGH);
+  digitalWrite(greenB, HIGH);
+
+  delay(delayTym); // Delay for 1000 milliseconds
+
+  digitalWrite(redA, HIGH);
+  digitalWrite(greenA, HIGH);
+  digitalWrite(redB, LOW);
+  digitalWrite(greenB, LOW);
+
+  delay(delayTym); // Delay for 1000 milliseconds
+
+  digitalWrite(redA, HIGH);
+  digitalWrite(greenA, HIGH);
+  digitalWrite(redB, LOW);
+  digitalWrite(greenB, HIGH);
+
+  delay(delayTym); // Delay for 1000 milliseconds
+
+  digitalWrite(redA, HIGH);
+  digitalWrite(greenA, HIGH);
+  digitalWrite(redB, HIGH);
+  digitalWrite(greenB, LOW);
+
+  delay(delayTym); // Delay for 1000 milliseconds
+
+  digitalWrite(redA, HIGH);
+  digitalWrite(greenA, HIGH);
+  digitalWrite(redB, HIGH);
+  digitalWrite(greenB, HIGH);
+
+  delay(delayTym); // Delay for 1000 milliseconds
+}
